f06684c2eccc7d49a3aa8af478bcfdf2767686bf
cOrlbr <carl.tms2@gmail.com>
2025-04-21 23:16:12 +0800

    Test Restore

---
+++ src/main/java/org/yl/oop_web/controller/ApplicationController.java
@@ -0,0 +1,10 @@
+package org.yl.oop_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/applications")
+public class ApplicationController {
+    // Other application-related methods can be added here in the future
+}
--- src/main/java/org/yl/oop_web/model/SeminarWorkshop.java
+++ src/main/java/org/yl/oop_web/model/SeminarWorkshop.java
@@ -9,27 +9,29 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor // Add this annotation for a no-args constructor
 @Entity
-@Table(name = "scholarships")
-public class Scholarship {
+@Table(name = "seminar_workshop") // Change the table name as needed
+public class SeminarWorkshop {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String title;
     private String description;
+    private String date;
     private String link;
     private String imagePath;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id") // Assuming a user can create a seminar/workshop
     private User user;
 
     // Constructor for easy instantiation
-    public Scholarship(String name, String description, String link) {
-        this.name = name;
+    public SeminarWorkshop(String title, String description, String date, String link) {
+        this.title = title;
         this.description = description;
+        this.date = date;
         this.link = link;
-        this.imagePath = "https://i.imgur.com/0000000.png";
+        this.imagePath = "https://i.imgur.com/0000000.png"; // Default image path
     }
 }
