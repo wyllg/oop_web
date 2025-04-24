b73ff74eb1e635c4d8ba203ca6eaca49b0585a32
mimiku1 <23-05723@g.batstate-u.edu.ph>
2025-04-24 12:40:49 +0800

    update InternshipApplicationService

---
+++ src/main/java/org/yl/oop_web/controller/InternshipApplicationController.java
@@ -0,0 +1,13 @@
+package org.yl.oop_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/internship/applications")
+public class InternshipApplicationController {
+
+    // Basic structure to handle future functionality for internship applications
+    // You can add additional methods for viewing, applying, etc. here
+
+}
