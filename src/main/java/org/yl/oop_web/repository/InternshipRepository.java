76e7b2b6e45f7c1f0730b9f1ed4ddf6315e8ca0b
mimiku1 <23-05723@g.batstate-u.edu.ph>
2025-04-24 12:02:54 +0800

    update InternshipRepository

---
+++ src/main/java/org/yl/oop_web/repository/InternshipRepository.java
@@ -0,0 +1,7 @@
+package org.yl.oop_web.repository;
+
+import org.yl.oop_web.model.Internship;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InternshipRepository extends JpaRepository<Internship, Long> {
+}
